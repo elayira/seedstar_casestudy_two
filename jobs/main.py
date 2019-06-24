eec4bf2227940738649931253f168ca330e1229d
Ighor Jesse <38922270+elayira@users.noreply.github.com>
2019-06-24 03:11:34 +0100

    Update main.py

--- jobs/main.py
+++ jobs/main.py
@@ -1,9 +1,10 @@
 from .utility import query_jobs, save_jobs
+import settings
 
 
 if __name__ == '__main__':
 
-    jobs = query_jobs()
+    jobs = query_jobs(settings.url)
     save_jobs(jobs)
     print('job data save completed')
                
